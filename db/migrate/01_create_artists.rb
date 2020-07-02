2eb0e46ac518d6f0bee56ea9dd3daadf01aba88d
Kayla Kingston <kkingston09@gmail.com>
2020-07-02 13:14:51 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,4 +1,5 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
+<<<<<<< HEAD
     def change
         create_table :artists do |t|
             t.string :name
@@ -9,3 +10,11 @@ class CreateArtists < ActiveRecord::Migration[5.2]
     end
 end
 
+=======
+  def up 
+  end
+  
+  def down
+  end 
+end
+>>>>>>> 29c3b32e1e3d8ce6a1c6ef5e1a4aac0e1d80dee0
